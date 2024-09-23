f4fa86d040979204c9e4e83fd4cd111ea0bd09b8
AskaSlip <varenaska@gmail.com>
2024-09-23 22:18:00 +0400

    hw6 modified

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,8 @@
 import { ApiError } from "../errors/api.errors";
+import { ITokenPayload } from "../interfaces/token.interface";
 import { IUser } from "../interfaces/user.interface";
 import { userRepository } from "../repositories/user.repository";
+import { passwordService } from "./password.service";
 
 class UserService {
   public async getList(): Promise<IUser[]> {
@@ -8,7 +10,9 @@ class UserService {
   }
 
   public async create(dto: Partial<IUser>): Promise<IUser> {
-    return await userRepository.create(dto);
+    await this.isEmailExistOrThrow(dto.email);
+    const password = await passwordService.hashPassword(dto.password);
+    return await userRepository.create({ ...dto, password });
   }
 
   public async getById(userId: string): Promise<IUser> {
@@ -19,11 +23,26 @@ class UserService {
     return user;
   }
 
-  public async update(userId: string, dto: IUser): Promise<IUser> {
-    return await userRepository.update(userId, dto);
+  public async getMe(jwtPayload: ITokenPayload): Promise<IUser> {
+    const user = await userRepository.getById(jwtPayload.userId);
+    if (!user) {
+      throw new ApiError("User not found", 404);
+    }
+    return user;
   }
-  public async delete(userId: string): Promise<void> {
-    return await userRepository.delete(userId);
+
+  public async updateMe(jwtPayload: ITokenPayload, dto: IUser): Promise<IUser> {
+    return await userRepository.update(jwtPayload.userId, dto);
+  }
+  public async deleteMe(jwtPayload: ITokenPayload): Promise<void> {
+    return await userRepository.delete(jwtPayload.userId);
+  }
+
+  private async isEmailExistOrThrow(email: string): Promise<void> {
+    const user = await userRepository.getByEmail(email);
+    if (user) {
+      throw new ApiError("Email already exists", 409);
+    }
   }
 }
 export const userService = new UserService();
