1dd5da0c89908b35768b6ddd6f3873939719298e
AskaSlip <varenaska@gmail.com>
2024-09-23 22:17:20 +0400

    hw6 modified

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,28 +1,31 @@
 import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 import { commonMiddleware } from "../middlewares/common.middleware";
+import { UserValidator } from "../services/user.validator";
 
 const router = Router();
 
 router.get("/", userController.getList);
-router.post("/", commonMiddleware.isBodyValid, userController.create);
+// router.post(
+//   "/",
+//   commonMiddleware.isBodyValid(UserValidator.create),
+//   userController.create,
+// );
 
+router.get("/me", authMiddleware.checkAccessToken, userController.getMe);
+router.put(
+  "/me",
+  authMiddleware.checkAccessToken,
+  commonMiddleware.isBodyValid(UserValidator.update),
+  userController.updateMe,
+);
+router.delete("/me", authMiddleware.checkAccessToken, userController.deleteMe);
 router.get(
   "/:userId",
   commonMiddleware.isIdValid("userId"),
   userController.getById,
-);
-router.put(
-  "/:userId",
-  commonMiddleware.isIdValid("userId"),
-  commonMiddleware.isBodyValid,
-  userController.update,
-);
-router.delete(
-  "/:userId",
-  commonMiddleware.isIdValid("userId"),
-  userController.delete,
 );
 
 export const userRouter = router;
--- src/services/user.validator.ts
+++ src/services/user.validator.ts
@@ -1,45 +1,53 @@
 import Joi from "joi";
 
-export const schema = Joi.object({
-  name: Joi.string().min(2).alphanum().required().messages({
+import { regexConstant } from "../constants/regex.constants";
+
+export class UserValidator {
+  private static name = Joi.string().trim().min(3).alphanum().messages({
     "string.base": "Name should be a type of text.",
     "string.empty": "Name cannot be empty.",
-    "string.min": "Name should have a minimum length of 2.",
+    "string.min": "Name should have a minimum length of 3.",
     "string.alphanum": "Name should only contain alphanumeric characters.",
     "any.required": "Name is a required field.",
-  }),
+  });
 
-  password: Joi.string()
-    .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-    .min(6)
-    .required()
+  private static password = Joi.string()
+    .regex(regexConstant.PASSWORD)
+    .trim()
     .messages({
-      "string.pattern.base":
-        "Password must only contain alphanumeric characters.",
       "string.min": "Password should have a minimum length of 6 characters.",
       "any.required": "Password is a required field.",
-    }),
+    });
 
-  email: Joi.string()
+  private static email = Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required()
+    .trim()
+    .regex(regexConstant.EMAIL)
     .messages({
       "string.email": "Please provide a valid email address.",
       "any.required": "Email is a required field.",
-    }),
+    });
 
-  age: Joi.number().min(1).max(116).integer().messages({
+  private static age = Joi.number().min(18).max(116).integer().messages({
     "number.base": "Age must be a number.",
-    "number.min": "Age must be at least 1.",
+    "number.min": "Age must be at least 18.",
     "number.max": "Age must be less than or equal to 116.",
     "number.integer": "Age must be an integer.",
-  }),
+  });
 
-  phone: Joi.string().allow("+").min(9).max(13),
+  private static phone = Joi.string().trim().regex(regexConstant.PHONE);
 
-  role: Joi.string(),
+  public static create = Joi.object({
+    name: this.name.required(),
+    age: this.age.required(),
+    phone: this.phone,
+    password: this.password.required(),
+    email: this.email.required(),
+  });
 
-  isVerified: Joi.boolean(),
-
-  isDeleted: Joi.boolean(),
-});
+  public static update = Joi.object({
+    name: this.name.required(),
+    age: this.age.required(),
+    phone: this.phone,
+  });
+}
